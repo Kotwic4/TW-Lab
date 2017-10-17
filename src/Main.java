77d8d7669147172fc3492c85e0e1d8c17aff6e38
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-10-17 14:30:14 +0200

    lab 3 schema

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import lab1.Lab1;
 import lab2.Lab2;
+import lab3.Lab3;
 
 public class Main {
 
     public static void main(String[] args) {
-        Lab1.main();
+        //Lab1.main();
+        //Lab2.main();
+        Lab3.main();
     }
 }
