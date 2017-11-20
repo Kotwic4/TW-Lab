86ab785fbd09ae345c463a79a943fbcb47bec431
Radomir Krawczykiewicz <kotwic4@gmail.com>
2017-11-20 20:30:41 +0100

    lab 5 finish

--- src/lab5/Lab5.java
+++ src/lab5/Lab5.java
@@ -1,18 +1,78 @@
 package lab5;
 
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class Lab5
 {
     public static void main(){
-        Mandelbrot mandelbrot = null;
-        try {
-            mandelbrot = new Mandelbrot(800);
-            mandelbrot.setVisible(true);
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        int[] maxIters = {800,200,100};
+        int[] threadNumbers = {400, 200, 100 , 10};
+        for(int maxIter : maxIters){
+            for(int threadNumber : threadNumbers){
+                test(maxIter,threadNumber);
+            }
+            test(maxIter);
         }
+    }
+
+    private static double getMean(List<Long> values)
+    {
+        double sum = 0.0;
+        for(double x : values)
+            sum += x;
+        return sum/values.size();
+    }
+
+    private static double getVariance(List<Long> values)
+    {
+        double mean = getMean(values);
+        double temp = 0;
+        for(double x :values)
+            temp += (x-mean)*(x-mean);
+        return temp/(values.size()-1);
+    }
+
+    private static double getStdDev(List<Long> values)
+    {
+        return Math.sqrt(getVariance(values));
+    }
+
+    private static void test(int maxIter, int threadNumber){
+        List<Long> results = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            Long start = System.nanoTime();
+            try {
+                Mandelbrot mandelbrot = new Mandelbrot(maxIter,threadNumber);
+                mandelbrot.setVisible(true);
+                mandelbrot.dispose();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Long end = System.nanoTime();
+            results.add(end-start);
+        }
+        System.out.println(String.format("maxIter: %d, threadNumber: %d, mean: %f, dev: %f",
+                maxIter,threadNumber,getMean(results),getStdDev(results)));
+    }
+
+    private static void test(int maxIter){
+        List<Long> results = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            Long start = System.nanoTime();
+            try {
+                Mandelbrot mandelbrot = new Mandelbrot(maxIter);
+                mandelbrot.setVisible(true);
+                mandelbrot.dispose();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Long end = System.nanoTime();
+            results.add(end-start);
+        }
+        System.out.println(String.format("maxIter: %d, threadNumber: single, mean: %f, dev: %f",
+                maxIter,getMean(results),getStdDev(results)));
     }
 }
--- src/lab5/Mandelbrot.java
+++ src/lab5/Mandelbrot.java
@@ -2,6 +2,7 @@ package lab5;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -12,32 +13,55 @@ import javax.swing.*;
 
 public class Mandelbrot extends JFrame {
 
-    private final int MAX_ITER = 570;
-    private final double ZOOM = 150;
+    private final double ZOOM = 200;
     private BufferedImage I;
     private int witdh = 800;
     private int height = 600;
 
-    public Mandelbrot(int max_iter) throws ExecutionException, InterruptedException {
+    public Mandelbrot(int maxIter, int threadsNumber) throws ExecutionException, InterruptedException {
         super("Mandelbrot Set");
         setBounds(0, 0, witdh, height);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        ExecutorService pool = Executors.newFixedThreadPool(1000);
+        ExecutorService pool = Executors.newFixedThreadPool(threadsNumber);
         I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
-        List<List<Future<Integer>>> values = new LinkedList<>();
+        List<List<Future<Integer>>> values = new ArrayList<>();
         for (int y = 0; y < getHeight(); y++) {
-            List<Future<Integer>> list = new LinkedList<>();
+            List<Future<Integer>> list = new ArrayList<>();
             for (int x = 0; x < getWidth(); x++) {
-                Future<Integer> val = pool.submit(new MandelbrotWorker(x,y,MAX_ITER,witdh,height,ZOOM));
+                Future<Integer> val = pool.submit(new MandelbrotWorker(x,y,maxIter,witdh,height,ZOOM));
                 list.add(val);
             }
             values.add(list);
         }
         for (int y = 0; y < getHeight(); y++) {
             for (int x = 0; x < getWidth(); x++) {
-                int iter = values.get(y).get(x).get();
-                I.setRGB(x, y, iter | (iter << 8));
+                int val = values.get(y).get(x).get();
+                I.setRGB(x, y, val | (val << 8));
+            }
+        }
+    }
+
+    public Mandelbrot(int maxIter) throws ExecutionException, InterruptedException {
+        super("Mandelbrot Set");
+        setBounds(0, 0, witdh, height);
+        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ExecutorService pool = Executors.newSingleThreadExecutor();
+        I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        List<List<Future<Integer>>> values = new ArrayList<>();
+        for (int y = 0; y < getHeight(); y++) {
+            List<Future<Integer>> list = new ArrayList<>();
+            for (int x = 0; x < getWidth(); x++) {
+                Future<Integer> val = pool.submit(new MandelbrotWorker(x,y,maxIter,witdh,height,ZOOM));
+                list.add(val);
+            }
+            values.add(list);
+        }
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                int val = values.get(y).get(x).get();
+                I.setRGB(x, y, val | (val << 8));
             }
         }
     }
